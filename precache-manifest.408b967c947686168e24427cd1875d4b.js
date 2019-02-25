878ded063d2a3545040f25be97c4b54b24a496c8
Lawrence <lawrencejiang@berkeley.edu>
2019-02-24 16:43:31 -0800

    Updates

---
+++ precache-manifest.408b967c947686168e24427cd1875d4b.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c6c705f78a9d5d96f9293bf171c9496",
+    "url": "/static/media/software_logo.9c6c705f.png"
+  },
+  {
+    "revision": "fb49357e215e6b0314bd",
+    "url": "/static/css/main.d71cfc92.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "72fa9ce51a2792aa566f",
+    "url": "/static/js/2.72fa9ce5.chunk.js"
+  },
+  {
+    "revision": "99b5cece16c047146faf62cab2439091",
+    "url": "/static/media/design_box.99b5cece.png"
+  },
+  {
+    "revision": "9702eef3b44212fd5165d6711b8f7bc7",
+    "url": "/static/media/mulan_icon.9702eef3.png"
+  },
+  {
+    "revision": "9a15f4f52e8d9a461b7ee53335668b05",
+    "url": "/static/media/mulanzhao_logo.9a15f4f5.png"
+  },
+  {
+    "revision": "c8b51b289f247a44f318d58131440408",
+    "url": "/static/media/box_yellow.c8b51b28.png"
+  },
+  {
+    "revision": "fb49357e215e6b0314bd",
+    "url": "/static/js/main.fb49357e.chunk.js"
+  },
+  {
+    "revision": "1ea72955a48bd98179c55991547b36f1",
+    "url": "/static/media/upper_left.1ea72955.png"
+  },
+  {
+    "revision": "9e3fd6bbd9e1631fb1be37c8b642306d",
+    "url": "/static/media/bottom_left.9e3fd6bb.png"
+  },
+  {
+    "revision": "983a609e45fc5e84b696093f6328f60c",
+    "url": "/static/media/upper_right.983a609e.png"
+  },
+  {
+    "revision": "c12434c30290b81148439cddf6e13423",
+    "url": "/static/media/bottom_right.c12434c3.png"
+  },
+  {
+    "revision": "5e36f53e5df929dffeb65333b4fe572c",
+    "url": "/static/media/design_logo.5e36f53e.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fc5ee7498bb66bf95e7000963c1c465a",
+    "url": "/index.html"
+  }
+];
